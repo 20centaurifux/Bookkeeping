4db4bb1eded788488f4115b695ad1c34d3db6e03
lordkefir <lord-kefir@arcor.de>
2013-12-03 16:48:50 +0100

    update application info (version & email)

--- src/accounting/AppInfo.java
+++ src/accounting/AppInfo.java
@@ -21,7 +21,7 @@ public final class AppInfo
 	public static String TITLE = "Bookkeeping";
 	public static int VERSION_MAJOR = 0;
 	public static int VERSION_MINOR = 1;
-	public static int VERSION_PATCHLEVEL = 0;
+	public static int VERSION_PATCHLEVEL = 1;
 	public static String AUTHOR = "Sebsatian Fedrau";
-	public static String AUTHOR_EMAIL = "lord-kefir@arcor.de";
+	public static String AUTHOR_EMAIL = "sebastian.fedrau@gmail.com";
 }
